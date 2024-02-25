f2459736b19dcd70ca1dc8d89aafc19c308e5216
robertocalabrese <r.calabrese@edicomspa.it>
2024-02-25 18:47:20 +0100

    Fix tabella

--- pages/[regione]/[provincia].vue
+++ pages/[regione]/[provincia].vue
@@ -1,84 +1,144 @@
 <template>
   <v-container>
-    <h3 class="display-1 text-center mb-6">Presidi Medici di Emergenza a {{ provincia }}</h3>
-    <v-row >
-      <v-col v-for="(presidio, key) in presidi" :key="key" cols="12" sm="6" md="6">
-        <v-card class="mb-6" elevation="3" :title="presidio.nome">
-          <v-card-text>
-            <p><strong>Tipo:</strong> {{ presidio.type }}</p>
-            <p><strong>Indirizzo:</strong> {{ presidio.indirizzo }}</p>
-            <p><strong>Telefono:</strong> {{ presidio.telefono }}</p>
-            <p><strong>Email:</strong> {{ presidio.email }}</p>
-            <p><strong>Web:</strong> <a :href="presidio.web" target="_blank">Link</a></p>
-            <p><strong>Coordinate:</strong> Lat {{ presidio.coords.lat }}, Lng {{ presidio.coords.lng }}</p>
-            <v-divider class="my-4"></v-divider>
-            <v-progress-linear
-                :active="!presidio.data.data"
-                color="primary"
-                indeterminate
-                rounded
-                height="6"
-            ></v-progress-linear>
-            <div v-if="presidio.data">
-              <h3 class="subtitle-1 font-weight-bold">Situazione Attuale</h3>
-              <v-list density="compact">
-                <v-list-item v-for="(value, key) in presidio.data.data" :key="key" v-show="key !== 'extra'">
-                  <v-list-item v-if="key !== 'extra'">
-                    <v-list-item-title><strong>{{ key }}:</strong> {{ value.value }}</v-list-item-title>
-                    <v-list-item-subtitle v-for="(extraValue, extraKey) in value.extra" :key="extraKey">
-                      <strong>{{ extraValue.label }}:</strong> {{ extraValue.value }}
-                    </v-list-item-subtitle>
-                  </v-list-item>
-                </v-list-item>
-              </v-list>
+    <h2 class="display-1 text-center mb-6">Presidi Medici di Emergenza a {{ uppercaseFirstLetter(provincia) }}</h2>
 
-              <v-card-actions v-if="presidio.data.data">
-                <v-btn
-                    :prepend-icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-                    @click="show = !show"
-                >Extra</v-btn>
-              </v-card-actions>
+    <template v-for="categoria in ospedali">
+      <v-card flat v-if="categoria.data">
+        <v-card-title class="d-flex align-center pe-2">
+          <v-icon :icon="categoria.icon"></v-icon> &nbsp;
+          Pronto Soccorsi {{ categoria.titolo }}
 
-              <v-expand-transition>
-                <div v-show="show">
-                  <v-divider></v-divider>
-                  <template v-for="(value, key) in presidio.data.data" :key="key">
-                    <v-card-text v-show="key === 'extra'" >
-                      <p v-for="(extraValue, extraKey) in value" :key="extraKey">
-                        <strong>{{ extraValue.label }}: </strong>{{ extraValue.value }}
-                      </p>
-                    </v-card-text>
-                  </template>
-                </div>
-              </v-expand-transition>
+          <v-spacer></v-spacer>
+
+          <v-text-field
+              v-model="categoria.search.value"
+              prepend-inner-icon="mdi-magnify"
+              density="compact"
+              label="Cerca"
+              single-line
+              flat
+              hide-details
+              variant="solo-filled"
+          ></v-text-field>
+        </v-card-title>
+
+        <v-divider></v-divider>
+        <v-data-table
+            v-model:search="categoria.search"
+            :headers="headers"
+            :items="categoria.data"
+        >
+          <template v-slot:item.data.data.rosso.value="{ item }">
+            <div v-if="!item.data.data?.rosso?.value">
+              <v-progress-circular
+                  indeterminate
+                  color="red"
+              ></v-progress-circular>
             </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+            <h2 v-else class="text-red">{{ item.data.data?.rosso?.value }}</h2>
+          </template>
+
+          <template v-slot:item.data.data.giallo.value="{ item }">
+            <div v-if="!item.data.data?.giallo?.value">
+              <v-progress-circular
+                  indeterminate
+                  color="yellow"
+              ></v-progress-circular>
+            </div>
+            <h2 v-else class="text-yellow">{{ item.data.data?.giallo?.value }}</h2>
+          </template>
+
+
+          <template v-slot:item.data.data.verde.value="{ item }">
+            <div v-if="!item.data.data?.verde?.value">
+              <v-progress-circular
+                  indeterminate
+                  color="green"
+              ></v-progress-circular>
+            </div>
+            <h2 v-else class="text-green">{{ item.data.data?.verde?.value }}</h2>
+          </template>
+
+          <template v-slot:item.data.data.bianco.value="{ item }">
+            <div v-if="!item.data.data?.bianco?.value">
+              <v-progress-circular
+                  indeterminate
+                  color="white"
+              ></v-progress-circular>
+            </div>
+            <h2 v-else>{{ item.data.data?.bianco?.value }}</h2>
+          </template>
+
+        </v-data-table>
+      </v-card>
+
+      <v-divider color="success" class="my-8"></v-divider>
+
+    </template>
+
   </v-container>
 </template>
 
 <script setup lang="ts">
 const runtimeConfig = useRuntimeConfig();
-const { regione, provincia } = useRoute().params
+const {regione, provincia} = useRoute().params
 
+const search = ref('')
 
-const { data, error, pending } = await useAPIFetch<ResultsType>(`/${regione}/${provincia}`, {
+const headers = [
+  {title: 'Pronto Soccorso', align: 'start', key: 'nome'},
+  {title: 'Codice Rosso', align: 'end', key: 'data.data.rosso.value'},
+  {title: 'Codice Giallo', align: 'end', key: 'data.data.giallo.value'},
+  {title: 'Codice Verde', align: 'end', key: 'data.data.verde.value'},
+  {title: 'Codice Bianco', align: 'end', key: 'data.data.bianco.value'},
+];
+
+const sortBy = ref([{key: 'data.data.rosso.value', order: 'desc'}])
+
+const {data, error, pending} = await useAPIFetch<ResultsType>(`/${regione}/${provincia}`, {
   method: "GET",
 })
 
 const presidi = data.value.data
 
-const show = ref(false);
+console.log(presidi);
+
+let ospedaliPediatrici = presidi.filter(obj => obj.type === "pediatrico");
+let ospedaliAdulti = presidi.filter(obj => obj.type === "adulti");
+
+let ospedali = [
+  {
+    titolo: 'Pediatrici',
+    icon: 'mdi-human-baby-changing-table',
+    data: ospedaliPediatrici,
+    search: ref('')
+  },
+  {
+    titolo: 'Adulti',
+    icon: 'mdi-account',
+    data: ospedaliAdulti,
+    search: ref('')
+  }
+]
+
+console.log(ospedaliAdulti);
+
 
 console.log(runtimeConfig.public.pusher.schema);
 
 window.pusher.subscribe(data.value.websocket.channel).bind(data.value.websocket.event, (data) => {
   console.log('Evento ricevuto:', data);
   for (const [key, value] of Object.entries(data.data)) {
-    presidi[key].data = value
+    for (const [k, presidio] of Object.entries(presidi)) {
+      if (presidio.key === key) {
+        presidi[k].data = value
+      }
+    }
   }
 });
+
+function uppercaseFirstLetter(value: string) {
+  return value.charAt(0).toUpperCase() + value.slice(1);
+}
 
 </script>
