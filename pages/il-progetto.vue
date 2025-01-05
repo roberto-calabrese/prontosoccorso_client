0c735366b3c00dc98a58974a52c26ce4fa375187
robertocalabrese <r.calabrese@edicomspa.it>
2025-01-05 08:48:23 +0100

    Fix pagina progetto colori

--- pages/il-progetto.vue
+++ pages/il-progetto.vue
@@ -18,7 +18,7 @@
       <v-row>
         <v-col cols="12" md="8" offset-md="2">
           <v-card>
-            <v-card-title class="headline text-amber-accent-2"><v-icon icon="mdi-human-greeting-variant"></v-icon> Introduzione</v-card-title>
+            <v-card-title class="headline text-1"><v-icon icon="mdi-human-greeting-variant"></v-icon> Introduzione</v-card-title>
             <v-card-text>
               Benvenuti su <strong>Pronto Soccorso Live</strong>, un progetto nato dalla passione per la tecnologia e per offrire un servizio utile alla comunità. Qui troverai una panoramica in tempo reale dello stato dei pronto soccorso in Italia, suddiviso per provincia. Tutti i dati provengono da fonti istituzionali ufficiali il cui obiettivo è rendere queste informazioni facilmente accessibili a tutti.
             </v-card-text>
@@ -30,7 +30,7 @@
       <v-row class="mt-10" id="contribute">
         <v-col cols="12" md="8" offset-md="2">
           <v-card>
-            <v-card-title class="headline text-amber-accent-2"><v-icon icon="mdi-code-json"></v-icon> Codice Sorgente e Collaborazione</v-card-title>
+            <v-card-title class="headline text-1"><v-icon icon="mdi-code-json"></v-icon> Codice Sorgente e Collaborazione</v-card-title>
             <v-card-text>
               <p>Questo progetto è ancora work in progress e stiamo cercando supporto per arricchire ulteriormente la nostra banca dati. Se hai competenze o risorse che potrebbero essere utili, il tuo contributo sarebbe molto apprezzato!</p>
               Il codice sorgente del progetto è disponibile su <v-icon color="orange">mdi-github</v-icon> <a href="https://github.com/roberto-calabrese" target="_blank">GitHub</a>. Se sei uno sviluppatore, un designer, o semplicemente un appassionato, ti invito a unirti per migliorare il progetto. Ogni contributo è ben accetto!
@@ -43,7 +43,7 @@
       <v-row class="mt-10">
         <v-col cols="12" md="8" offset-md="2">
           <v-card>
-            <v-card-title class="headline text-amber-accent-2"><v-icon icon="mdi-database-eye-off-outline"></v-icon> Trasparenza e Privacy</v-card-title>
+            <v-card-title class="headline text-1"><v-icon icon="mdi-database-eye-off-outline"></v-icon> Trasparenza e Privacy</v-card-title>
             <v-card-text>
               <p><strong>Pronto Soccorso Live</strong> non intende sostituire le informazioni ufficiali. I dati non vengono salvati permanentemente e non vengono raccolte informazioni sugli utenti. Viene utilizzata una cache temporanea per ridurre le richieste alle fonti, e Google Analytics per monitorare le visite, in modo del tutto anonimo.</p>
             </v-card-text>
@@ -55,7 +55,7 @@
       <v-row class="mt-10">
         <v-col cols="12" md="8" offset-md="2">
           <v-card>
-            <v-card-title class="headline text-amber-accent-2"><v-icon icon="mdi-aws"></v-icon>Tecnologie e Costi</v-card-title>
+            <v-card-title class="headline text-1"><v-icon icon="mdi-aws"></v-icon>Tecnologie e Costi</v-card-title>
             <v-card-text>
               Questo progetto utilizza tecnologie moderne e un'infrastruttura ospitata su AWS. Nonostante sia sviluppato nel tempo libero e senza scopo di lucro, ci sono dei costi da sostenere, come il dominio e le risorse cloud. Il tuo supporto può fare la differenza.
             </v-card-text>
@@ -67,7 +67,7 @@
       <v-row class="mt-10">
         <v-col cols="12" md="8" offset-md="2">
           <v-card>
-            <v-card-title class="headline text-amber-accent-2"><v-icon icon="mdi-weight-lifter"></v-icon>Sostieni il Progetto</v-card-title>
+            <v-card-title class="headline text-1"><v-icon icon="mdi-weight-lifter"></v-icon>Sostieni il Progetto</v-card-title>
             <v-card-text>
               Se trovi utile questo servizio e desideri che continui a crescere, considera una donazione su <v-icon color="orange">mdi-coffee</v-icon> <a href="https://buymeacoffee.com/roberto.calabrese?l=it" target="_blank">Buy Me a Coffee</a>. Il tuo contributo aiuterà a coprire i costi e a migliorare il progetto.
             </v-card-text>
