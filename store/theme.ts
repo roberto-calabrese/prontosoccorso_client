abf2e44a137beb8a311e542546686e2f52e8e3ac
robertocalabrese <r.calabrese@edicomspa.it>
2025-01-05 08:46:44 +0100

    Fix tema default

--- store/theme.ts
+++ store/theme.ts
@@ -1,4 +1,3 @@
-// stores/themeStore.js
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 import { useTheme } from 'vuetify'
@@ -6,22 +5,16 @@ import { useTheme } from 'vuetify'
 export const useThemeStore = defineStore('theme', () => {
     const theme = useTheme()
 
-    // Stato iniziale basato su localStorage o un valore di default
-    const currentTheme = ref(localStorage.getItem('theme') || 'customLightTheme')
+    const currentTheme = ref(localStorage.getItem('theme') || 'customDarkTheme')
 
-    // Metodo per cambiare tema
     const toggleTheme = () => {
         currentTheme.value = currentTheme.value === 'customLightTheme' ? 'customDarkTheme' : 'customLightTheme'
         theme.global.name.value = currentTheme.value
-
-        // Memorizza in localStorage
         localStorage.setItem('theme', currentTheme.value)
     }
 
-    // Imposta il tema all'avvio
     theme.global.name.value = currentTheme.value
 
-    // Icona dinamica basata sul tema
     const themeIcon = computed(() => {
         return currentTheme.value === 'customLightTheme' ? 'mdi-weather-night' : 'mdi-weather-sunny'
     })
